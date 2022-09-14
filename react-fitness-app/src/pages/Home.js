0318fb90f9dacb2ee66fd20bc5f5756c49e6846f
aditya0802 <adityanegandhi02@gmail.com>
2022-09-14 20:18:54 +0530

    created hero-banner component for home page

--- react-fitness-app/src/pages/Home.js
+++ react-fitness-app/src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
+import SearchExcercises from '../components/SearchExcercises';
+import Excercises from '../components/Excercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExcercises />
+      <Excercises />
+    </Box>
   )
 }
 
